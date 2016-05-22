d9e6ad0f0e5b796d6b6b2bd39d720e797f54f535
Jakub Pastuszek <jpastuszek@gmail.com>
2016-05-22 18:27:01 +0100

    using chain! instead of tuple!

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -15,37 +15,43 @@ use Val;
 use NamedValues;
 use ip;
 use raw;
-use nom::{IResult, Needed, be_u16};
+use nom::{IResult, Needed, be_u16, rest};
 
 pub fn dissect(data : &[u8]) -> DissectResult {
-    let mut values = NamedValues::new();
 
     //TODO: beter parsing: 802.1Q tag, minimum payload size, CRC
-    match tuple!(data, take!(6), take!(6), be_u16) {
-        IResult::Done(remainder, (dest, src, tlen)) => {
-            values.push(("Destination", Val::Bytes(dest)));
-            values.push(("Source", Val::Bytes(src)));
+    match chain!(data,
+                 dest: take!(6) ~
+                 src: take!(6) ~
+                 tlen: be_u16 ~
+                 remainder: rest,
+                 || {
+                    let mut values = NamedValues::new();
 
-            if tlen <= 1500 {
-                values.push(("Length", Val::Unsigned(tlen as u64)));
-            } else {
-                match tlen {
-                    0x800 => values.push(("IP", Val::Payload(ip::dissect(remainder)))),
-                    0x806 => values.push(("ARP", Val::Payload(raw(remainder)))),
-                    0x8138 => values.push(("IPX", Val::Payload(raw(remainder)))),
-                    0x86dd => values.push(("IPv6", Val::Payload(raw(remainder)))),
-                    _ => values.push(("Unknown Type", Val::Payload(Err(DissectError::InvalidData(format!["unknown protocol: {:x}", tlen]))))),
-                };
-            };
-        },
-        IResult::Incomplete(Needed::Size(needed)) =>
-            return Err(DissectError::Underflow { expected: needed, have: data.len(),
-                message: format!("An Ethernet frame must be at least {} B", needed) }),
+                    values.push(("Destination", Val::Bytes(dest)));
+                    values.push(("Source", Val::Bytes(src)));
+
+                    if tlen <= 1500 {
+                        values.push(("Length", Val::Unsigned(tlen as u64)));
+                    } else {
+                        match tlen {
+                            0x800 => values.push(("IP", Val::Payload(ip::dissect(remainder)))),
+                            0x806 => values.push(("ARP", Val::Payload(raw(remainder)))),
+                            0x8138 => values.push(("IPX", Val::Payload(raw(remainder)))),
+                            0x86dd => values.push(("IPv6", Val::Payload(raw(remainder)))),
+                            _ => values.push(("Unknown Type", Val::Payload(Err(DissectError::InvalidData(format!["unknown protocol: {:x}", tlen]))))),
+                        };
+                    };
+
+                    values
+                 }) {
+        IResult::Done(_, values) => Ok(Box::new(Val::Object(values))),
+        IResult::Incomplete(Needed::Size(needed)) => Err(DissectError::Underflow {
+            expected: needed, have: data.len(),
+            message: format!("An Ethernet frame must be at least {} B", needed) }),
         //TODO: anything I can do here?
         _ => panic!("failed to parse Ethernet packet!")
     }
-
-    Ok(Box::new(Val::Object(values)))
 }
 
 #[cfg(test)]
@@ -54,14 +60,14 @@ mod test {
 
     #[test]
     fn dissect_ethernet() {
-        let data = [132, 56, 53, 69, 73, 136, 156, 32, 123, 233, 26, 2, 8, 0];
+        let data = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xa0, 0x0b, 0xba, 0x84, 0x2d, 0x0e, 0x08, 0x06, 0x00, 0x01, 0x08, 0x00, 0x06, 0x04, 0x00, 0x01, 0xa0, 0x0b, 0xba, 0x84, 0x2d, 0x0e, 0xc0, 0xa8, 0x01, 0x7c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0xa8, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 
         let val = *dissect(&data).unwrap();
         println!("{}", &val.pretty_print(0));
 
-        assert_eq!(val["Destination"].as_bytes().unwrap(), &[0x84, 0x38, 0x35, 0x45, 0x49, 0x88]);
-        assert_eq!(val["Source"].as_bytes().unwrap(), &[0x9c, 0x20, 0x7b, 0xe9, 0x1a, 0x02]);
-        assert!(val["IP"].as_payload().unwrap().is_err())
+        assert_eq!(val["Destination"].as_bytes().unwrap(), &[0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+        assert_eq!(val["Source"].as_bytes().unwrap(), &[0xa0, 0x0b, 0xba, 0x84, 0x2d, 0x0e]);
+        assert!(val["ARP"].is_payload());
     }
 
     #[test]
